bdbc995a51159527c8500c6a2635ee59b03167d8
Conor Timmis <mctto@protonmail.com>
2024-09-21 19:54:01 +0000

    Route App to render SignUp Form

--- betteryou/src/App.js
+++ betteryou/src/App.js
@@ -2,6 +2,8 @@ import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Routes } from "react-router-dom";
+import './api/axiosDefaults';
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<h1>Home page</h1>} />
           <Route exact path="/signin" element={<h1>Sign in</h1>} />
-          <Route exact path="/signup" element={<h1>Sign up</h1>} />
+          <Route exact path="/signup" render ={() =><SignUpForm />} />
           <Route path="*" element={<p>Page not found!</p>} />
         </Routes>
       </Container>
