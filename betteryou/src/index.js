ace15913eb08f7eefbd668b7ab7dea0e63eea8df
Conor Timmis <mctto@protonmail.com>
2024-09-22 19:47:27 +0000

    Refactor index.js

--- betteryou/src/index.js
+++ betteryou/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
     </Router>
   </React.StrictMode>
 );
