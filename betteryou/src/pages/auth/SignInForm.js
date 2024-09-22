ea9c538f9f2d68b03b9a70d97e49e07cf6c4c7db
Conor Timmis <mctto@protonmail.com>
2024-09-22 14:57:42 +0000

    Update SignInForm route and imports

--- betteryou/src/pages/auth/SignInForm.js
+++ betteryou/src/pages/auth/SignInForm.js
@@ -1,16 +1,7 @@
 import React, { useState } from "react";
-import axios from "axios"
-
-import Form from "react-bootstrap/Form";
-import Alert from "react-bootstrap/Alert";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Image from "react-bootstrap/Image";
-import Container from "react-bootstrap/Container";
-
-import { useNavigate } from "react-router-dom";
-
+import axios from "axios";
+import { Form, Button, Col, Row, Image, Container } from "react-bootstrap";
+import { useNavigate, Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
@@ -23,56 +14,60 @@ function SignInForm() {
   const { username, password } = signInData;
 
   const navigate = useNavigate();
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       await axios.post("/dj-rest-auth/login/", signInData);
       navigate("/");
     } catch (err) {
+      // Handle error (e.g., show an alert)
     }
   };
+
   const handleChange = (event) => {
     setSignInData({
       ...signInData,
       [event.target.name]: event.target.value,
     });
   };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign in</h1>
+        <Container className={`${appStyles.Content} p-4`}>
+          <h1 className={styles.Header}>Sign In</h1>
           <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="username">
-                    <Form.Label className="d-none">Username</Form.Label>
-                    <Form.Control 
-                        type="text" 
-                        placeholder="Username"
-                        name="username" 
-                        className={styles.Input}
-                        value={username}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                className={styles.Input}
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
-                <Form.Group controlId="password">
-                    <Form.Label className="d-none">Password</Form.Label>
-                    <Form.Control 
-                        type="password" 
-                        placeholder="Password" 
-                        name="password"
-                        className={styles.Input}
-                        value={password}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                <Button 
-                    className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-                    type="submit"
-                >
-                    Sign In
-                </Button>
-            </Form>
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                className={styles.Input}
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign In
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
