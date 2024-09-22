d14cd26202f5a9ef34c559c81b3e95444f898984
Conor Timmis <mctto@protonmail.com>
2024-09-22 19:48:30 +0000

    Refactor SignInForm page

--- betteryou/src/pages/auth/SignInForm.js
+++ betteryou/src/pages/auth/SignInForm.js
@@ -5,8 +5,11 @@ import { useNavigate, Link } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
+  const setCurrentUser = useSetCurrentUser();
+
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
@@ -16,11 +19,12 @@ function SignInForm() {
   const [errors, setErrors] = useState({});
 
   const navigate = useNavigate();
-  
   const handleSubmit = async (event) => {
     event.preventDefault();
+
     try {
-      await axios.post("/dj-rest-auth/login/", signInData);
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
       navigate("/");
     } catch (err) {
       setErrors(err.response?.data);
