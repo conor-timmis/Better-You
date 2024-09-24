3fe99eaf821947ac66c3f2285010013a34f2a665
Conor Timmis <mctto@protonmail.com>
2024-09-24 15:16:05 +0000

    Add PostCreateForm Route to App

--- betteryou/src/App.js
+++ betteryou/src/App.js
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<h1>Home page</h1>} />
           <Route path="/signin" element={<SignInForm />} />
           <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/posts/create" element={<PostCreateForm />} />
           <Route path="*" element={<p>Page not found!</p>} />
         </Routes>
       </Container>
