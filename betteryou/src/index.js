68fd68272def0dae4f51a6f4d3ba890abf907908
Conor Timmis <mctto@protonmail.com>
2024-09-26 20:01:22 +0000

    Change index.js with downgrade of react-router-dom

--- betteryou/src/index.js
+++ betteryou/src/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <CurrentUserProvider>
         <App />
       </CurrentUserProvider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
