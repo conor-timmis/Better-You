f1e8c24847589bb2128f0e78f5c08a2d1843cf3e
Conor Timmis <mctto@protonmail.com>
2024-10-01 17:05:54 +0000

    Add ProfileDataProvider and use createRoot API

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
+import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <CurrentUserProvider>
-        <App />
-      </CurrentUserProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <CurrentUserProvider>
+    <ProfileDataProvider>
+      <App />
+    </ProfileDataProvider>
+  </CurrentUserProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
