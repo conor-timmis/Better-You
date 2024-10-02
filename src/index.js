c92d5a953929bc7c01122584394ff2d5053eb420
Conor Timmis <mctto@protonmail.com>
2024-10-02 16:26:25 +0000

    Refactor index.js to use createRoot and Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import "./index.css";
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
@@ -8,9 +11,13 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <CurrentUserProvider>
-    <ProfileDataProvider>
-      <App />
-    </ProfileDataProvider>
-  </CurrentUserProvider>
+  <Router>
+    <CurrentUserProvider>
+      <ProfileDataProvider>
+        <App />
+      </ProfileDataProvider>
+    </CurrentUserProvider>
+  </Router>
 );
+
+reportWebVitals();
