95e9a0c7faa10bfcc850caca788eb6ed2da9a557
Conor Timmis <mctto@protonmail.com>
2024-10-06 17:11:58 +0000

    Update babel.config with newer transform plugin

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    presets: ["react-app"],
-    plugins: ["@babel/plugin-proposal-private-property-in-object"]
-  };
-  
+  presets: ["react-app"],
+  plugins: ["@babel/plugin-transform-private-property-in-object"]
+};
