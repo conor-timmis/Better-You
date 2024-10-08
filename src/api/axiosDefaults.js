7b86bd6d1e64bee266c57474373e6065c4336883
Conor Timmis <mctto@protonmail.com>
2024-10-08 15:40:49 +0000

    Change baseURL to deployed backend

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "/api";
+axios.defaults.baseURL = "https://better-you-ec0aa381f182.herokuapp.com/";
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
